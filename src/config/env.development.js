996fc53ba3be3fd8fb7e4cbe5781bcf7a9f1c543
linqunxun <aidadaimadelinqunxun@gmail.com>
2021-05-21 17:50:38 +0800

    update:修改项目名称

--- src/config/env.development.js
+++ src/config/env.development.js
@@ -1,6 +1,6 @@
 // 本地环境配置
 module.exports = {
-  title: 'vue-h5-template',
+  title: 'weebot_h5_front',
   baseUrl: 'http://engineering.linqunxun.com', // 项目地址
   baseApi: 'http://localhost:12346', // 本地api请求地址,注意：如果你使用了代理，请设置成'/'
   APPID: 'xxx',
