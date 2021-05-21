3a4f31d342f6ba67d4a1e910716316b8986c7a81
linqunxun <aidadaimadelinqunxun@gmail.com>
2021-05-21 17:58:15 +0800

    update:优化页面，去掉不需要的代码

--- src/views/customer/auth.vue
+++ src/views/customer/auth.vue
@@ -7,44 +7,30 @@ import {authByCustomer, getAuthUrl} from "api/wx_mp";
 let config = require('../../config');
 
 export default {
-  data() {
-    return {
-
-    }
-  },
-
-  computed: {},
-
   mounted() {
-    this.authorize();
-  },
+    let params = new URLSearchParams(location.search);
+    let uid = params.get("uid");
+    let ak = params.get("ak");
+    let code = params.get("code");
+    let that = this;
 
-  methods: {
-    authorize(){
-      let params = new URLSearchParams(location.search);
-      let uid = params.get("uid");
-      let ak = params.get("ak");
-      let code = params.get("code");
-      let that = this;
+    if(!uid || !ak){
+      alert("请使用正确的授权链接");
+      return
+    }
 
-      if(!uid || !ak){
-        alert("请使用正确的授权链接");
-        return
-      }
-
-      if(code){
-        authByCustomer({uid: uid, ak: ak, code: code})
-          .then(res => that.$router.replace(`/customer?ak=${ak}`))
-          .catch(err => {
-            alert(err.msg);
-          });
-      }else {
-        getAuthUrl({redirect: `${config.baseUrl}/customer/auth?uid=${uid}&ak=${ak}`})
-          .then(res => window.location.href = res.result)
-          .catch(err => {
-            alert(err.msg);
-          });
-      }
+    if(code){
+      authByCustomer({uid: uid, ak: ak, code: code})
+        .then(res => that.$router.replace(`/customer?ak=${ak}`))
+        .catch(err => {
+          alert(err.msg);
+        });
+    }else {
+      getAuthUrl({redirect: `${config.baseUrl}/customer/auth?uid=${uid}&ak=${ak}`})
+        .then(res => window.location.href = res.result)
+        .catch(err => {
+          alert(err.msg);
+        });
     }
   }
 }
--- src/views/employee/auth.vue
+++ src/views/employee/auth.vue
@@ -5,47 +5,32 @@
 <script>
 import {authByEmployee, getAuthUrl} from "api/wx_mp";
 let config = require('../../config');
-let auth = authByEmployee;
 
 export default {
-  data() {
-    return {
-
-    }
-  },
-
-  computed: {},
-
   mounted() {
-    this.authorize();
-  },
+    let params = new URLSearchParams(location.search);
+    let eid = params.get("eid");
+    let ak = params.get("ak");
+    let code = params.get("code");
+    let that = this;
 
-  methods: {
-    authorize(){
-      let params = new URLSearchParams(location.search);
-      let eid = params.get("eid");
-      let ak = params.get("ak");
-      let code = params.get("code");
-      let that = this;
+    if(!eid || !ak){
+      alert("请使用正确的授权链接");
+      return
+    }
 
-      if(!eid || !ak){
-        alert("请使用正确的授权链接");
-        return
-      }
-
-      if(code){
-        auth({eid: eid, ak: ak, code: code})
-          .then(res => that.$router.replace({name:"Home"}))
-          .catch(err => {
-            alert(err.msg);
-          });
-      }else {
-        getAuthUrl({redirect: `${config.baseUrl}/employee/auth?eid=${eid}&ak=${ak}`})
-          .then(res => window.location.href = res.result)
-          .catch(err => {
-            alert(err.msg);
-          });
-      }
+    if(code){
+      authByEmployee({eid: eid, ak: ak, code: code})
+        .then(res => alert("授权成功，请关闭当前页面！"))
+        .catch(err => {
+          alert(err.msg);
+        });
+    }else {
+      getAuthUrl({redirect: `${config.baseUrl}/employee/auth?eid=${eid}&ak=${ak}`})
+        .then(res => window.location.href = res.result)
+        .catch(err => {
+          alert(err.msg);
+        });
     }
   }
 }
